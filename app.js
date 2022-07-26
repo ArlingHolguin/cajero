2b5c027e775ba79e465d8ee95f88d4e630d84639
Arling Holguin <arlingholguin@gmail.com>
2022-07-26 09:12:24 -0500

    Ajustes -Pendiente por finalizar
    Ajustes -Pendiente por finalizar
    
    Co-Authored-By: arlingholguin <54909429+arlingholguin@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-// arreglo de objetos cuentas 
+// arreglo de objetos cuentas
 let cuentas = [{
-        "nombre": "Mali",
-        "saldo": 200,
-        "email": "mali.mali@email.com"
+        nombre: "Mali",
+        saldo: 200,
+        email: "mali.mali@email.com",
     },
     {
-        "nombre": "Gera",
-        "saldo": 290,
-        "email": "gera.gera@email.com"
+        nombre: "Gera",
+        saldo: 290,
+        email: "gera.gera@email.com",
     },
     {
-        "nombre": "Maui",
-        "saldo": 67,
-        "email": "maui.maui@email.com"
+        nombre: "Maui",
+        saldo: 67,
+        email: "maui.maui@email.com",
     },
     {
-        "nombre": "Arling",
-        "saldo": 500,
-        "email": "arlin.holguin@email.com"
+        nombre: "Arling",
+        saldo: 500,
+        email: "arlin.holguin@email.com",
     },
     {
-        "nombre": "David",
-        "saldo": 550,
-        "email": "david.zonana@email.com"
-    }
+        nombre: "David",
+        saldo: 550,
+        email: "david.zonana@email.com",
+    },
 ];
 
 //recorro el aarreglo de objetos cuentas y muestro el nombre y el saldo
@@ -34,26 +34,56 @@ let cuentas = [{
 
 // }
 
+
+
+document.querySelector("#botonCuentas").addEventListener("click", traerCuentas);
+
+function traerCuentas() {
+    // console.log("dentro de la fucnion");
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "cuentas.json", true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let cuentas = JSON.parse(this.responseText);
+            let res = document.querySelector("#res");
+            res.innerHTML = "";
+            let nombre = prompt("Ingresa su nombre");
+            let password = prompt("ingresa tu clave");
+
+            for (let cuenta of cuentas) {
+                if (nombre == cuenta.nombre && password == cuenta.password) {
+                    res.innerHTML = `<p class="my-2 font-bold text-upercase bg-green-500 p-1 rounded text-center">${cuenta.nombre} bienvenido a nuestro cajero.</p><tr>
+                    <td><button class="btn btn-info btn-sm">Consultar saldo</button>
+                    <button class="btn btn-primary btn-sm">Ingresar Monto</button>
+                    <button class="btn btn-success btn-sm">Retirar Monto</button> 
+                    <button class="btn btn-warning btn-sm">Salir</button></td>
+                    <tr>`;
+
+                    let botonCuentas = document.querySelector("#botonCuentas");
+                    botonCuentas.style.display = "none";
+                }
+            }
+        }
+    };
+}
+
 //recorre el arreglo de objetos cuentas y muestro el nombre y el saldo
-cuentas.map(function(cuenta) {
-    console.log(cuenta.nombre + " tiene un saldo de: " + cuenta.saldo);
-    document.getElementById('cta').innerHTML = `<p class="mr-1">Cuenta ${cuenta.nombre}  </p>
-    <input class="mt-1 mr-4" type="radio">`;
-});
+// cuentas.map(function(cuenta) {
+//     console.log(cuenta.nombre + " tiene un saldo de: " + cuenta.saldo);
+//     document.getElementById('cta').innerHTML = `<p class="mr-1">Cuenta ${cuenta.nombre}  </p>
+//     <input class="mt-1 mr-4" type="radio">`;
+// });
 
-
-
-//buscar cuenta por nombre 
-let cuenta = cuentas.find(cuenta => cuenta.nombre === "Arling");
-alert(cuenta.saldo + " es el saldo de " + cuenta.nombre);
-console.log(cuenta.saldo);
-
+//buscar cuenta por nombre
+// let cuenta = cuentas.find((cuenta) => cuenta.nombre === "Arling");
+// alert(cuenta.saldo + " es el saldo de " + cuenta.nombre);
+// console.log(cuenta.saldo);
 
 //acceder a el arreglo de objetos cuentas
 // let cuenta = cuentas.find(cuenta => cuenta.nombre === "mali");
 // alert(cuenta.saldo);
 // alert(cuentas[0].saldo);
-
 
 //Al seleccionar una cuenta, debes ingresar el password asociado a la cuenta.
 // Si el password es incorrecto, debes notificar al usuario y permitirle intentarlo
@@ -68,8 +98,7 @@ console.log(cuenta.saldo);
 // Al seleccionar retirar monto, el usuario debe escribir el monto a retirar. Al retirar el monto, debe mostrarle al usuario el monto retirado y el nuevo saldo total.
 // Como regla de negocio, una cuenta no debe de tener más de $990 y menos de $10. Es necesario hacer las validaciones pertinentes para que no se rompa esta regla de negocio.
 
-
-//generar md5 
+//generar md5
 // window.addEventListener('load', function() {
 //     var strHash = md5('mali.mali@email.com');
 //     // alert('The MD5 hash of the tutsplus string is:' + strHash);
